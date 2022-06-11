f7245fcec5f4ca2c02f19698a90992d617eff54d
Rachel Sanham <green_chul@hotmail.com>
2022-06-11 21:58:43 +0100

    change port in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 
-const APP_PORT = 3000;
+const APP_PORT = 4000;
 
 app.listen(APP_PORT, () => {
-  console.log("app is listening on port");
+  console.log(`app is listening on port ${APP_PORT}`);
 });
